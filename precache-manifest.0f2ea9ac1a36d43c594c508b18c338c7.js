3c2d0d1f6a98b5dc5e6e6a4d569b40571511db3b
Vedant Mehta <vedantmehta300@gmail.com>
2020-08-15 13:30:44 +0530

    Updates

---
+++ precache-manifest.0f2ea9ac1a36d43c594c508b18c338c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557a388b95477e73fe8d6fceca2e2b90",
+    "url": "/changer1/index.html"
+  },
+  {
+    "revision": "c6131bc92d78508623b5",
+    "url": "/changer1/static/css/main.a3ae2ee2.chunk.css"
+  },
+  {
+    "revision": "04a28bc4e12cf7ef29cd",
+    "url": "/changer1/static/js/2.b6595194.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/changer1/static/js/2.b6595194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6131bc92d78508623b5",
+    "url": "/changer1/static/js/main.644bcac0.chunk.js"
+  },
+  {
+    "revision": "176e41aef4c1ae3479d6",
+    "url": "/changer1/static/js/runtime-main.1b77c5f9.js"
+  }
+]);
